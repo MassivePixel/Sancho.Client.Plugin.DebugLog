54857728fa146da6356b3021510bb1c7acf5627d
Toni Petrina <tpetrina@tpetrina.com>
2016-07-28 13:33:03 +0200

    Added plugin for debug logging

---
+++ src/Sancho.Client.Plugin.DebugLog/DebugLogPlugin.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Massive Pixel.  All Rights Reserved.  Licensed under the MIT License (MIT). See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+using Sancho.Client.Core;
+
+namespace Sancho.Client.Plugin.DebugLog
+{
+    public class DebugLogPlugin : IPlugin
+    {
+        Connection connection;
+
+        public string Name => "plugin-debuglog";
+
+        public DebugLogPlugin(Connection connection)
+        {
+            if (connection == null)
+                throw new ArgumentNullException(nameof(connection));
+
+            this.connection = connection;
+        }
+
+        public void Recieve(Message message)
+        {
+            // this plugin doesn't receive any messages
+        }
+
+        public Task Log(string text) => connection.SendAsync(Name, "log", text);
+    }
+}
